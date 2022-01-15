2d35928a71a69e2238fdb2eb0d6f521c30c8866b
ihsnayvid <sri.divyanshi1920@gmail.com>
2022-01-15 11:38:06 +0530

    Day15 Check if a Pair with given difference is present

---
+++ D15_FindDifferencePair.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+bool findPair(int arr[], int size, int n){
+    int i=0,j=1;
+    sort(arr,arr+size);
+    while(i<size && j<size){
+       if((arr[j]-arr[i])==n) return true;
+       else if((arr[j]-arr[i])<n) j++;
+       else i++;
+    }
+    return false;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int l,n;
+        cin>>l>>n;
+        int arr[l];
+        for(int i=0;i<l;i++)
+            cin>>arr[i];
+        if(findPair(arr, l, n))
+            cout<<1<<endl;
+        else cout<<"-1"<<endl;
+    }  
+    return 0;
+}
+
+
